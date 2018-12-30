580b47c6cb81e64b02f68804e6a464f4ecc4af64
jcraver1021 <elciego@gmail.com>
2018-12-30 18:18:39 -0500

    includes for install

--- setup.py
+++ setup.py
@@ -7,8 +7,12 @@ setuptools.setup(
     name="downer",
     version="0.0.1",
     author="James Craver",
+	url='https://github.com/jcraver1021/downer.git',
     description="Bulk HTML downloader",
     packages=setuptools.find_packages(),
+	package_data={
+		'': ['*.txt'],
+	},
     classifiers=[
         "Programming Language :: Python :: 3",
     ],
